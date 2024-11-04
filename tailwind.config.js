e6c6c686eeacb5315b2169108cb202148c7ec754
H-BlaZe <ibnazmat79@gmail.com>
2024-11-04 17:16:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+    /** @type {import('tailwindcss').Config} */
+    module.exports = {
+    content: [
+        "./templates/**/*.html",
+        "./templates/**/*.html",
+    "./templates/**/*.js"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                pop:["Poppins", "sans-serif"],
+            }
+        }  // No custom colors
+    },
+    plugins: [],
+    }
+            
